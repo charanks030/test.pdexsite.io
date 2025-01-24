82b4b346909c3e5e51f6cdb53b74436a55929ef8
Sree Charan <charanks030@gmail.com>
2025-01-24 18:03:32 +0530

    im16

---
+++ _next/static/chunks/pages/v2-9268d608f7cabf9d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[615],{7821:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/v2",function(){return t(9932)}])},9932:function(n,u,t){"use strict";t.r(u),t.d(u,{default:function(){return i}});var _=t(5893);function i(){return(0,_.jsx)("div",{})}}},function(n){n.O(0,[888,774,179],function(){return n(n.s=7821)}),_N_E=n.O()}]);
